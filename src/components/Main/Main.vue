fc423e7cecfd4caec754fb34ac724c72671330f8
Vasya <vpetruin@inbox.ru>
2021-09-03 07:23:56 +0300

    Сверстан компопнент Main

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -20,5 +20,34 @@
         </p>
       </div>
     </section>
+    <section class="first-question">
+      <h2 class="first-question__title">Боитесь ли вы умереть?</h2>
+      <div class="first-question__btns-wrapper">
+        <button class="first-question__btn btn btn_color_yellow" type="button">
+          Да
+        </button>
+        <button class="first-question__btn btn btn_color_yellow" type="button">
+          Нет
+        </button>
+      </div>
+      <p class="quethion-num first-question__text">Вопрос 1-5</p>
+    </section>
+    <section class="quote">
+      <img
+        src="../../assets/image/eyes.svg"
+        alt="eye"
+        class="quote__eye-icon"
+      />
+      <img
+        src="../../assets/image/eye.svg"
+        alt="planet"
+        class="quote__planet"
+      />
+      <img src="../../assets/image/rune1.svg" alt="rune" class="quote__rune1" />
+      <p class="quote__text">
+        Вы, конечно, умрете. И все, с кем вы знакомы, тоже однажды умрут.
+      </p>
+      <img src="../../assets/image/rune2.svg" alt="rune" class="quote__rune2" />
+    </section>
   </main>
 </template>
