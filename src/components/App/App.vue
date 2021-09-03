caeeeafa72ca27b352e4288545f606278af4444c
Vasya <vpetruin@inbox.ru>
2021-09-03 07:55:10 +0300

    Сверстан компопнент Footer

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -2,18 +2,21 @@
   <div class="app">
     <Header />
     <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "../Header/Header.vue";
 import Main from "../Main/Main.vue";
+import Footer from "../Footer/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Main,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <p class="footer__text">
+      TERMENI SI CONDITII: ACESTA ESTE UN SERVICIU DE DIVERTISMENT. PRIN
+      FOLOSIREA LUI DECLARATI CA AVETI 18 ANI IMPLINITI,
+    </p>
+  </footer>
+</template>
